593597f80f35bde460e3d54416b5eeecd7cff2cc
Jia Ren <renj@udel.edu>
2015-07-29 23:52:02 -0400

    Jul29

---
+++ algorithms/46_permutations.py
@@ -0,0 +1,23 @@
+class Solution:
+    # @param {integer[]} nums
+    # @return {integer[][]}
+    def permute(self, nums):
+        if len(nums)==0:
+            return []
+        return self.permutation([[nums[0]]], nums[1:])
+            
+    def permutation(self, result, remain):
+        if len(remain) == 0:
+            return result
+        else:
+            res = []
+            v = remain[0]
+            for l in result:
+                for i in xrange(len(l)+1):
+                    new = l[:]
+                    new.insert(i, v)
+                    res.append(new)
+            return self.permutation(res, remain[1:])
+                    
+
+print Solution().permute([1,2,3])
