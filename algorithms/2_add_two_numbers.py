6fdecaf48a9faf08a9d6730dbbfc1a42c6bac88f
Jia Ren <renj@udel.edu>
2016-08-13 16:15:02 -0400

    Update 2_add_two_numbers.py
    Improve my prev solution. use linked list instead of flatten the data structure.

--- algorithms/2_add_two_numbers.py
+++ algorithms/2_add_two_numbers.py
@@ -27,3 +27,50 @@ class Solution:
             a.next = next
             next = a
         return next
+        
+# Aug13
+root = ListNode(0)
+        node = root
+        carry = 0
+        while l1 and l2:
+            digit = l1.val + l2.val + carry
+            if digit > 9:
+                digit = digit % 10
+                carry = 1
+            else:
+                carry = 0
+            node.next = ListNode(digit)
+            node = node.next
+            l1 = l1.next
+            l2 = l2.next
+        while l1:
+            if carry > 0:
+                digit = l1.val + carry
+                if digit > 9:
+                    digit = digit % 10
+                    carry = 1
+                else:
+                    carry = 0
+                node.next = ListNode(digit)
+                node = node.next
+                l1 = l1.next
+            else:
+                node.next = l1
+                break
+        while l2:
+            if carry > 0:
+                digit = l2.val + carry
+                if digit > 9:
+                    digit = digit % 10
+                    carry = 1
+                else:
+                    carry = 0
+                node.next = ListNode(digit)
+                node = node.next
+                l2 = l2.next
+            else:
+                node.next = l2
+                break
+        if carry > 0:
+            node.next = ListNode(carry)
+        return root.next
