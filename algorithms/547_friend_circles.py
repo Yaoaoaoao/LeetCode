8bebbdf03b969e4e508a4530bd83a5f6460cfb6b
Jia Ren <renj@udel.edu>
2017-06-08 20:32:13 -0400

    Jun08-2017

---
+++ algorithms/547_friend_circles.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def findCircleNum(self, M):
+        """
+        :type M: List[List[int]]
+        :rtype: int
+        """
+        # strongly connected component
+        m = len(M)
+        count = 0
+        queue = []
+        visited = [False] * m
+
+        def direct_friend(stu):
+            return [i for i in range(m) if M[stu][i] == 1 and not visited[i]]
+
+        for stu in range(m):
+            if not visited[stu]:
+                count += 1
+                visited[stu] = True
+                queue.extend(direct_friend(stu))
+                while len(queue) > 0:
+                    next_stu = queue.pop(0)
+                    if not visited[next_stu]:
+                        visited[next_stu] = True
+                        queue.extend(direct_friend(next_stu))
+        return count
+
+
+print Solution().findCircleNum([[1, 1, 0], [1, 1, 0], [0, 0, 1]])  # 2
+print Solution().findCircleNum([[1, 1, 0], [1, 1, 1], [0, 1, 1]])  # 1
